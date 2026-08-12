056c1ad16edc953c2757c6510b657f24dbcbdf11
Abdur Rahman <rahmannnm24@gmail.com>
2026-08-12 12:11:01 +0530

    Added a New Projects

--- src/data/project.js
+++ src/data/project.js
@@ -1,8 +1,10 @@
+import { image } from "motion/react-client";
+
 export const projectData = [
     {
         id:1,
         title: "Rest-O-Green stays Billing App",
-        description: "A comprehensive digital billing and management application. Features dynamic UI, responsive CSS layouts, and print-ready invoicing styled specifically for physical receipts.",
+        description: "A comprehensive management application featuring a dynamic UI, responsive layouts, and print-ready styling for physical receipts.",
         techStack: ["React.js", "CSS3", "Firebase Hosting"],
         image: "/BillingApp.jpeg",
         liveLink: "https://resort-billing-app.web.app/",
@@ -11,7 +13,7 @@ export const projectData = [
     {
         id: 2,
         title: "Nature Heaven Holidays Resort Booking",
-        description: "A full-stack booking system built for a freelance client. It streamlines room reservations and client management using a responsive React frontend and a cloud-hosted MongoDB/Node backend.",
+        description: "A full-stack application built with React and Node/MongoDB to streamline room reservations and client management for a freelance client",
         techStack: ["React", "Node.js", "Express", "MongoDB", "Vercel", "Render"],
         image: "/NatureHeavenHolidays.jpeg",
         liveLink: "https://natureheavenholidays.com/",
@@ -20,10 +22,28 @@ export const projectData = [
     {
         id: 3,
         title: "AI Resume Bulider and Interview Question",
-        description: "An intelligent application that uses the Groq AI model to generate professional, industry-standard resumes. Features a custom backend API to process inputs and automatically format data into polished CVs.",
+        description: "An AI-powered application using Groq AI and a custom backend API to automatically format and generate professional, industry-standard CVs.",
         techStack: ["React.js", "Node.js", "Express", "Groq AI"],
         image: "/AI_ResumeBuilder.png",
         liveLink: "#",
         githubLink: "#"
+    },
+    {
+        id: 4,
+        title: "Shop.co E-commerce",
+        description: "A fully responsive e-commerce storefront featuring dynamic product browsing, cart management, and a modern UI/UX design.",
+        techStack: ["Next.js", "React.js", "Tailwind css"],
+        image: "/Shop.co.png",
+        liveLink: "https://e-commerce-swart-theta-67.vercel.app/",
+        githubLink: "https://github.com/rahman24-dev/e-commerce"
+    },
+    {
+        id: 5,
+        title: "Visitor-Pass-Management",
+        description: "A full-stack application to completely digitize visitor tracking, check-ins, and approvals with secure role-based access.",
+        techStack: ["React.js", "Node.js", "Express.js", "MongoDB"],
+        image: "/Visitor-pass-management.png",
+        liveLink: "https://visitor-pass-five.vercel.app/",
+        githubLink: "https://github.com/rahman24-dev/visitor-pass-backend"
     }
 ]
